fe640e8d5286a7074308ac1ab872256c9c631154
Nuran Terlan <nuran.terlanli@asoiu.edu.az>
2021-01-29 04:02:20 +0400

    Employee model is created for testing

---
+++ FluentBInterface-RecursiveGenerics/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DesignPatterns
+{
+    public class Employee
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public short Age { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumMain { get; set; }
+        public string PhoneNumBackup { get; set; }
+        public Positions Position { get; set; }
+        public decimal Salary { get; set; }
+
+        public override string ToString()
+        {
+            return $"\n*************\nId: {Id}\n{FirstName} {LastName}, {Age}\n" +
+                   $"Email: {Email}\nMain phone number: {PhoneNumMain} | Backup: {PhoneNumBackup}\n" +
+                   $"Position: {Position}\nSalary: ${Salary}\n*************\\n";
+        }
+    }
+}
