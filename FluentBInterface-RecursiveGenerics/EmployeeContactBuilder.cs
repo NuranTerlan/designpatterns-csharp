f352c36221649ecbb277f7204822c83d0339974b
Nuran Terlan <nuran.terlanli@asoiu.edu.az>
2021-01-29 04:04:47 +0400

    Contact Info Builder of Employee Builder

---
+++ FluentBInterface-RecursiveGenerics/EmployeeContactBuilder.cs
@@ -0,0 +1,23 @@
+﻿namespace DesignPatterns
+{
+    public class EmployeeContactBuilder<T> : EmployeePersonalBuilder<EmployeeContactBuilder<T>> where T: EmployeeContactBuilder<T>
+    {
+        public T WithEMail(string email)
+        {
+            Employee.Email = email;
+            return (T) this;
+        }
+
+        public T WithMainPhone(string mainPhone)
+        {
+            Employee.PhoneNumMain = mainPhone;
+            return (T) this;
+        }
+
+        public T WithBackupPhone(string backupPhone)
+        {
+            Employee.PhoneNumBackup = backupPhone;
+            return (T) this;
+        }
+    }
+}
