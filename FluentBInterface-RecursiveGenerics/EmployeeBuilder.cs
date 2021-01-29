b0057c7d9fb68d8dc46dbe365c5b97a3a0013c6d
Nuran Terlan <nuran.terlanli@asoiu.edu.az>
2021-01-29 04:03:33 +0400

    Base Employee Builder created with Build method (which return last built entity)

---
+++ FluentBInterface-RecursiveGenerics/EmployeeBuilder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DesignPatterns
+{
+    public abstract class EmployeeBuilder
+    {
+        protected Employee Employee;
+
+        protected EmployeeBuilder()
+        {
+            Employee = new Employee
+            {
+                Id = Guid.NewGuid()
+            };
+        }
+
+        public Employee Build() => Employee;
+    }
+}
