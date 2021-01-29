eaa173f2a59eee367903b2bf7fe3bee8a4397535
Nuran Terlan <nuran.terlanli@asoiu.edu.az>
2021-01-29 04:04:57 +0400

    Job Info Builder of Employee Builder

---
+++ FluentBInterface-RecursiveGenerics/EmployeeJobBuilder.cs
@@ -0,0 +1,17 @@
+﻿namespace DesignPatterns
+{
+    public class EmployeeJobBuilder<T> : EmployeeContactBuilder<EmployeeJobBuilder<T>> where T: EmployeeJobBuilder<T>
+    {
+        public T AtPosition(Positions position)
+        {
+            Employee.Position = position;
+            return (T) this;
+        }
+
+        public T WithSalary(decimal salaryAmount)
+        {
+            Employee.Salary = salaryAmount;
+            return (T) this;
+        }
+    }
+}
