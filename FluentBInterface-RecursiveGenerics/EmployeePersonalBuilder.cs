7bc1d2ebe67d0005a1bc75c8b4f9985fea7e91a8
Nuran Terlan <nuran.terlanli@asoiu.edu.az>
2021-01-29 04:04:35 +0400

    Personal Info Builder of Employee Builder

---
+++ FluentBInterface-RecursiveGenerics/EmployeePersonalBuilder.cs
@@ -0,0 +1,23 @@
+﻿namespace DesignPatterns
+{
+    public class EmployeePersonalBuilder<T> : EmployeeBuilder where T: EmployeePersonalBuilder<T>
+    {
+        public T WithFirstName(string firstName)
+        {
+            Employee.FirstName = firstName;
+            return (T) this;
+        }
+
+        public T WithLastName(string lastName)
+        {
+            Employee.LastName = lastName;
+            return (T) this;
+        }
+
+        public T WithAge(short age)
+        {
+            Employee.Age = age;
+            return (T) this;
+        }
+    }
+}
