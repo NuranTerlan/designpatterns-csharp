93c4153c8f148800fe44cc176a385bec45cbb858
Nuran Terlan <nuran.terlanli@asoiu.edu.az>
2021-01-31 08:57:34 +0400

    Create CarInfoBuilder.cs

---
+++ FacetedB/CarInfoBuilder.cs
@@ -0,0 +1,34 @@
+﻿namespace DesignPatterns.FacetedB
+{
+    public class CarInfoBuilder : CarBuilderFacade
+    {
+        public CarInfoBuilder(Car car)
+        {
+            Car = car;
+        }
+
+        public CarInfoBuilder WithBrand(CarBrands brand)
+        {
+            Car.Brand = brand;
+            return this;
+        }
+
+        public CarInfoBuilder WithModel(string model)
+        {
+            Car.Model = model;
+            return this;
+        }
+
+        public CarInfoBuilder CreatedAt(int year)
+        {
+            Car.CreationYear = year;
+            return this;
+        }
+
+        public CarInfoBuilder WithDistance(double distance)
+        {
+            Car.DistanceWithKm = distance;
+            return this;
+        }
+    }
+}
