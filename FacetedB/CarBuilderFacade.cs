bde52159fe0e345a2cf5ae113b5ef24262a9eed0
Nuran Terlan <nuran.terlanli@asoiu.edu.az>
2021-01-31 08:57:19 +0400

    Create CarBuilderFacade.cs

---
+++ FacetedB/CarBuilderFacade.cs
@@ -0,0 +1,19 @@
+﻿namespace DesignPatterns.FacetedB
+{
+    public class CarBuilderFacade
+    {
+        public Car Car { get; set; }
+
+        public CarBuilderFacade()
+        {
+            Car = new Car();
+        }
+
+        public Car Build() => Car;
+
+        public CarInfoBuilder Info => new CarInfoBuilder(Car);
+        public CarEngineBuilder Engine => new CarEngineBuilder(Car);
+        public CarTradeBuilder Trade => new CarTradeBuilder(Car);
+        public CarAddressBuilder Address => new CarAddressBuilder(Car);
+    }
+}
