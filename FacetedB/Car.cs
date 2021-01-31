7b43de53115160c6c432fad44fef913a4c488161
Nuran Terlan <nuran.terlanli@asoiu.edu.az>
2021-01-31 08:56:47 +0400

    Car entity is created for Faceted Model Building

---
+++ FacetedB/Car.cs
@@ -0,0 +1,35 @@
+﻿namespace DesignPatterns.FacetedB
+{
+    public class Car
+    {
+        // General info
+        public CarBrands Brand { get; set; }
+        public string Model { get; set; }
+        public int CreationYear { get; set; }
+        public double DistanceWithKm { get; set; }
+
+        // About engine and it's features
+        public double Engine { get; set; }
+        public int HorsePower { get; set; }
+        public int Torque { get; set; }
+
+        // Trade info
+        public decimal Price { get; set; }
+        public bool IsSecondHand { get; set; }
+
+        // Address info
+        public string InCity { get; set; }
+        public string InDealer { get; set; }
+
+        public override string ToString()
+        {
+            return $"\n--------------\n" +
+                   $"{Brand} {Model} | {CreationYear} | ${Price}\n" +
+                   $"Engine Info: {Engine}, {HorsePower}Hp, {Torque}Nm\n" +
+                   $"Roadage: {DistanceWithKm}km\n" +
+                   $"Usage Status: {(IsSecondHand ? "Second Hand" : "New")}\n" +
+                   $"Address: {InDealer}, {InCity}" +
+                   $"\n--------------\n";
+        }
+    }
+}
