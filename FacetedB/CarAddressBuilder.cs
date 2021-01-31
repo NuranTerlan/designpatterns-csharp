903cbb9c53752acf6d58f638225ecb77796784b8
Nuran Terlan <nuran.terlanli@asoiu.edu.az>
2021-01-31 08:56:54 +0400

    Create CarAddressBuilder.cs

---
+++ FacetedB/CarAddressBuilder.cs
@@ -0,0 +1,22 @@
+﻿namespace DesignPatterns.FacetedB
+{
+    public class CarAddressBuilder : CarBuilderFacade
+    {
+        public CarAddressBuilder(Car car)
+        {
+            Car = car;
+        }
+
+        public CarAddressBuilder InCity(string city)
+        {
+            Car.InCity = city;
+            return this;
+        }
+
+        public CarAddressBuilder InDealer(string dealer)
+        {
+            Car.InDealer = dealer;
+            return this;
+        }
+    }
+}
