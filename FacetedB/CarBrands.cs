90ec04f604aa531e3c935cb508e9123cefb67590
Nuran Terlan <nuran.terlanli@asoiu.edu.az>
2021-01-31 08:57:07 +0400

    Car Brands Enumerator

---
+++ FacetedB/CarBrands.cs
@@ -0,0 +1,14 @@
+﻿namespace DesignPatterns.FacetedB
+{
+    public enum CarBrands
+    {
+        BMW,
+        Mercedes,
+        LandRover,
+        Toyota,
+        Hyundai,
+        Volkswagen,
+        Subaru,
+        Chevrolet
+    }
+}
