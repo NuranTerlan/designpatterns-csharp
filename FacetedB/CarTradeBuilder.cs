5a985afc91c22758bb9323e84cab39ec264ff9d2
Nuran Terlan <nuran.terlanli@asoiu.edu.az>
2021-01-31 08:57:38 +0400

    Create CarTradeBuilder.cs

---
+++ FacetedB/CarTradeBuilder.cs
@@ -0,0 +1,22 @@
+﻿namespace DesignPatterns.FacetedB
+{
+    public class CarTradeBuilder : CarBuilderFacade
+    {
+        public CarTradeBuilder(Car car)
+        {
+            Car = car;
+        }
+
+        public CarTradeBuilder WithPrice(decimal price)
+        {
+            Car.Price = price;
+            return this;
+        }
+
+        public CarTradeBuilder IsSecondHand(bool isSecondHand)
+        {
+            Car.IsSecondHand = isSecondHand;
+            return this;
+        }
+    }
+}
