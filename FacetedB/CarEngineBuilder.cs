3e8aaa0a901788f223546e12e33311b807b9feab
Nuran Terlan <nuran.terlanli@asoiu.edu.az>
2021-01-31 08:57:24 +0400

    Create CarEngineBuilder.cs

---
+++ FacetedB/CarEngineBuilder.cs
@@ -0,0 +1,28 @@
+﻿namespace DesignPatterns.FacetedB
+{
+    public class CarEngineBuilder : CarBuilderFacade
+    {
+        public CarEngineBuilder(Car car)
+        {
+            Car = car;
+        }
+
+        public CarEngineBuilder WithEngine(double engine = 2000.0)
+        {
+            Car.Engine = engine;
+            return this;
+        }
+
+        public CarEngineBuilder WithHorsePower(int horsePower)
+        {
+            Car.HorsePower = horsePower;
+            return this;
+        }
+
+        public CarEngineBuilder WithTorque(int torque)
+        {
+            Car.Torque = torque;
+            return this;
+        }
+    }
+}
