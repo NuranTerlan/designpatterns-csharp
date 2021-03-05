8edb21a5183ab3360601f94d26e383c809698a4e
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-03-06 02:08:42 +0400

    action manager

---
+++ FactoryMethod/WarmingManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DesignPatterns.FactoryMethod
+{
+    public class WarmingManager : IAirConditioner
+    {
+        private readonly double _temperature;
+
+        public WarmingManager(double temperature)
+        {
+            _temperature = temperature;
+        }
+
+        public void Operate()
+        {
+            Console.WriteLine($"Warming the room to the required temperature of {_temperature}°C");
+        }
+    }
+}
