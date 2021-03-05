a281d07a3ebea7208f0d6181b7faf30ec57ef01b
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-03-06 02:08:27 +0400

    action factory

---
+++ FactoryMethod/WarmingFactory.cs
@@ -0,0 +1,7 @@
+﻿namespace DesignPatterns.FactoryMethod
+{
+    public class WarmingFactory : AirConditionerFactory
+    {
+        public override IAirConditioner Create(double temperature) => new WarmingManager(temperature);
+    }
+}
