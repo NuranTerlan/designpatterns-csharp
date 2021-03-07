ad0af69c61aa88934b51715996855c2527760c57
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-03-07 23:52:18 +0400

    Create SingletonDataContainer.cs

---
+++ Singleton/SingletonDataContainer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace DesignPatterns.Singleton
+{
+    public class SingletonDataContainer : ISingletonContainer
+    {
+        private readonly Dictionary<string, int> _capitals;
+
+        private SingletonDataContainer()
+        {
+            Console.WriteLine("Initializing singleton object");
+            _capitals = new Dictionary<string, int>();
+
+            var elements = File.ReadAllLines("capitals.txt");
+            for (int i = 0; i < elements.Length; i += 2)
+            {
+                _capitals.Add(elements[i], int.Parse(elements[i + 1]));
+            }
+        }
+
+        public int GetPopulation(string name)
+        {
+            return _capitals[name];
+        }
+
+        private static readonly Lazy<SingletonDataContainer> _instance;
+
+        static SingletonDataContainer()
+        {
+            _instance = new Lazy<SingletonDataContainer>();
+        }
+
+        public static SingletonDataContainer Instance => _instance.Value;
+    }
+}
