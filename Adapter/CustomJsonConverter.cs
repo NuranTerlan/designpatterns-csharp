73c534caa1629ef04ece18285f601b51e5eab37a
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-03-12 23:03:47 +0400

    Create CustomJsonConverter.cs

---
+++ Adapter/CustomJsonConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Xml;
+
+namespace DesignPatterns.Adapter
+{
+    public class CustomJsonConverter
+    {
+        private readonly IEnumerable<Manufacturer> _manufacturers;
+
+        public CustomJsonConverter(IEnumerable<Manufacturer> manufacturers)
+        {
+            _manufacturers = manufacturers;
+        }
+
+        public string ConvertToJson()
+        {
+            var manufacturers = JsonSerializer.Serialize(_manufacturers, new JsonSerializerOptions
+            {
+                WriteIndented = true
+            });
+
+            Console.WriteLine("\nManufacturers List as JSON =>");
+            Console.WriteLine(manufacturers);
+
+            return manufacturers;
+        }
+    }
+}
