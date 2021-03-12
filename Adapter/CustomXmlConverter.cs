52f7038178feceb80d1531d05524ef5e2553ce99
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-03-12 23:03:49 +0400

    Create CustomXmlConverter.cs

---
+++ Adapter/CustomXmlConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace DesignPatterns.Adapter
+{
+    public class CustomXmlConverter
+    {
+        public XDocument GetXml()
+        {
+            var xDocument = new XDocument();
+            var manufacturers = new XElement("Manufacturers");
+            var manufacturerAttrs = ManufacturerDataProvider.GetData()
+                .Select(m => new XElement("Manufacturer",
+                    new XAttribute("City", m.City),
+                    new XAttribute("Name", m.Name),
+                    new XAttribute("Year", m.Year)));
+
+            manufacturers.Add(manufacturerAttrs);
+            xDocument.Add(manufacturers);
+
+            Console.WriteLine(xDocument);
+
+            return xDocument;
+        }
+    }
+}
