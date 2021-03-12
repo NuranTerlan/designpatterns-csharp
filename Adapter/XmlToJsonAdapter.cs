b8bfd440a2951e55fd7dd8720b558cbaa93abb06
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-03-12 23:04:00 +0400

    Create XmlToJsonAdapter.cs

---
+++ Adapter/XmlToJsonAdapter.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using System.Text.Json.Serialization;
+using Convert = System.Convert;
+
+namespace DesignPatterns.Adapter
+{
+    public class XmlToJsonAdapter : IXmlToJson
+    {
+        private readonly CustomXmlConverter _xmlConverter;
+
+        public XmlToJsonAdapter(CustomXmlConverter xmlConverter)
+        {
+            _xmlConverter = xmlConverter;
+        }
+
+        public void ConvertXmlToJson()
+        {
+            var manufacturers = _xmlConverter.GetXml()
+                .Element("Manufacturers")
+                ?.Elements("Manufacturer")
+                .Select(m => new Manufacturer
+                {
+                    City = m.Attribute("City")?.Value,
+                    Name = m.Attribute("Name")?.Value,
+                    Year = Convert.ToInt32(m.Attribute("Year")?.Value)
+                });
+
+            new CustomJsonConverter(manufacturers).ConvertToJson();
+        }
+    }
+}
