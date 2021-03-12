52ea23abe3fe831a9922d5829506baa8b941fd9d
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-03-12 23:03:55 +0400

    Create Manufacturer.cs

---
+++ Adapter/Manufacturer.cs
@@ -0,0 +1,9 @@
+﻿namespace DesignPatterns.Adapter
+{
+    public class Manufacturer
+    {
+        public string Name { get; set; }
+        public string City { get; set; }
+        public int Year { get; set; }
+    }
+}
