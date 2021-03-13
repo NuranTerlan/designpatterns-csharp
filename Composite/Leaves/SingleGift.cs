233cd7446049bd10ebdf047c7d28fbd9741211bc
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-03-13 23:11:46 +0400

    Create SingleGift.cs

---
+++ Composite/Leaves/SingleGift.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using DesignPatterns.Composite.Components;
+
+namespace DesignPatterns.Composite.Leaves
+{
+    public class SingleGift : GiftBase
+    {
+        public SingleGift(string name, int price) : base(name, price)
+        {
+        }
+
+        public override int CalculateTotalPrice()
+        {
+            Console.WriteLine($"{this.name} -> ${price}");
+
+            return price;
+        }
+    }
+}
