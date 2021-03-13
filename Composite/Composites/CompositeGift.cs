d0601ac01a3b2a594b9d3576ca29d40c7c5856b5
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-03-13 23:11:44 +0400

    Create CompositeGift.cs

---
+++ Composite/Composites/CompositeGift.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using DesignPatterns.Composite.Components;
+
+namespace DesignPatterns.Composite.Composites
+{
+    public class CompositeGift : GiftBase, IGiftOperations
+    {
+        private readonly IList<GiftBase> _gifts;
+
+        public CompositeGift(string name, int price = 0) : base(name, price)
+        {
+            _gifts = new List<GiftBase>();
+        }
+
+        public override int CalculateTotalPrice()
+        {
+            int total = 0;
+
+            Console.WriteLine($"{this.name} contains the following products with prices: ");
+
+            foreach (var gift in _gifts)
+            {
+                total += gift.CalculateTotalPrice();
+            }
+
+            Console.WriteLine("--------------");
+
+            return total;
+        }
+
+        public void Add(GiftBase gift)
+        {
+            _gifts.Add(gift);
+        }
+
+        public void Remove(GiftBase gift)
+        {
+            _gifts.Remove(gift);
+        }
+    }
+}
