3ec990e3c91a032a3e627d98b0f2312baf112fab
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-03-14 22:56:39 +0400

    Create OrderDecorator.cs

---
+++ Decorator/OrderDecorator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DesignPatterns.Decorator
+{
+    public class OrderDecorator : OrderBase
+    {
+        protected OrderBase Order;
+
+        public OrderDecorator(OrderBase order)
+        {
+            Order = order;
+        }
+
+        public override double CalculateTotalOrderPrice()
+        {
+            Console.WriteLine("Calculating the total price in a decorator class");
+            return Order.CalculateTotalOrderPrice();
+        }
+    }
+}
