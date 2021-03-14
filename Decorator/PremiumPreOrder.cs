bb2e8ce35d0166adf33ebdfe37808c730d3ebcd7
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-03-14 22:56:41 +0400

    Create PremiumPreOrder.cs

---
+++ Decorator/PremiumPreOrder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DesignPatterns.Decorator
+{
+    public class PremiumPreOrder : OrderDecorator
+    {
+        public PremiumPreOrder(OrderBase order) : base(order)
+        {
+        }
+
+        public override double CalculateTotalOrderPrice()
+        {
+            Console.WriteLine($"Calculating the total price in the {nameof(PremiumPreOrder)} class");
+            var preOrderPrice = base.CalculateTotalOrderPrice();
+
+            Console.WriteLine("Adding additional discount to a pre-order price for premium users");
+            return preOrderPrice * 0.9;
+        }
+    }
+}
