72ef670657df5b8fb9dc73879790ece7909629c5
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-03-14 22:56:35 +0400

    Create PreOrder.cs

---
+++ Decorator/PreOrder.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace DesignPatterns.Decorator
+{
+    public class PreOrder : OrderBase
+    {
+        public override double CalculateTotalOrderPrice()
+        {
+            Console.WriteLine("Calculating the total price of a pre-order");
+            // 10% discount applied
+            return this.Products.Sum(p => p.Price) * 0.9;
+        }
+    }
+}
