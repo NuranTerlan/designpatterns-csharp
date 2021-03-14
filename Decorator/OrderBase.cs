df7b24e011a6b5a1d2ea45b574feb36dc3bc9c89
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-03-14 22:56:26 +0400

    Create OrderBase.cs

---
+++ Decorator/OrderBase.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace DesignPatterns.Decorator
+{
+    public abstract class OrderBase
+    {
+        protected IList<Product> Products = new List<Product>
+        {
+            new Product {Name = "Phone", Price = 587},
+            new Product {Name = "Tablet", Price = 800},
+            new Product {Name = "PC", Price = 1200}
+        };
+
+        public abstract double CalculateTotalOrderPrice();
+    }
+}
