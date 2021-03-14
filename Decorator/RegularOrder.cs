7c6f4347d9319c7c9c2df343910f66729be53310
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-03-14 22:56:32 +0400

    Create RegularOrder.cs

---
+++ Decorator/RegularOrder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace DesignPatterns.Decorator
+{
+    public class RegularOrder : OrderBase
+    {
+        public override double CalculateTotalOrderPrice()
+        {
+            Console.WriteLine("Calculating the total price of a regular order");
+            return this.Products.Sum(p => p.Price);
+        }
+    }
+}
