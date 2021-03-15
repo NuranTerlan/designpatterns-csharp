496737e66e3ab967008b5e1c576ea801f3f94682
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-03-15 23:05:52 +0400

    Create ProductCommand.cs

---
+++ Command/ProductCommand.cs
@@ -0,0 +1,30 @@
+﻿using DesignPatterns.Command.Receivers;
+
+namespace DesignPatterns.Command
+{
+    public class ProductCommand : ICommand
+    {
+        private readonly Product2 _product;
+        private readonly PriceAction _priceAction;
+        private readonly int _amount;
+
+        public ProductCommand(Product2 product, PriceAction priceAction, int amount)
+        {
+            _product = product;
+            _priceAction = priceAction;
+            _amount = amount;
+        }
+
+
+        public void ExecuteAction()
+        {
+            if (_priceAction == PriceAction.Increase)
+            {
+                _product.IncreasePrice(_amount);
+                return;
+            }
+
+            _product.DecreasePrice(_amount);
+        }
+    }
+}
