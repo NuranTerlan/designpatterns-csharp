0cc2a1a3d85e2d4a068ad8d4ebd5a66aec458066
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-03-15 23:05:55 +0400

    Create Product2.cs

---
+++ Command/Receivers/Product2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace DesignPatterns.Command.Receivers
+{
+    public class Product2
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+
+        public Product2(string name, int price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public void IncreasePrice(int amount)
+        {
+            Price += amount;
+            Console.WriteLine($"The price for the {Name} has been increased by ${amount}");
+        }
+
+        public void DecreasePrice(int amount)
+        {
+            if (amount < Price)
+            {
+                Price -= amount;
+                Console.WriteLine($"The price for the {Name} has been decreased by ${amount}");
+            }
+        }
+
+        public override string ToString() => $"Current price for the {Name} product is ${Price}";
+    }
+}
