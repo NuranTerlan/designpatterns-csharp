57e59b8b4a2cf080647ad8949ffae788b0721285
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-03-15 23:05:45 +0400

    Create ModifyPrice.cs

---
+++ Command/Invokers/ModifyPrice.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace DesignPatterns.Command.Invokers
+{
+    public class ModifyPrice
+    {
+        private readonly IList<ProductCommand> _commands;
+        private ProductCommand _command;
+
+        public ModifyPrice()
+        {
+            _commands = new List<ProductCommand>();
+        }
+
+        public void SetCommand(ProductCommand command) => _command = command;
+
+        public void Invoke()
+        {
+            _commands.Add(_command);
+            _command.ExecuteAction();
+        }
+    }
+}
