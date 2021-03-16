0d068857e5a95ae6399f09d852c4ea5f770b3fca
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-03-16 23:02:19 +0400

    Create DeveloperReport.cs

---
+++ Strategy/DeveloperReport.cs
@@ -0,0 +1,13 @@
+﻿namespace DesignPatterns.Strategy
+{
+    public class DeveloperReport
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DeveloperLevel Level { get; set; }
+        public int WorkingHours { get; set; }
+        public double HourlyRate { get; set; }
+
+        public double CalculateSalary() => WorkingHours * HourlyRate;
+    }
+}
