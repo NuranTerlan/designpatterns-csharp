9f6063d8422b0bdda91643ef5ce9072a70face90
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-03-16 23:02:14 +0400

    strategy pattern is implemented

--- Program.cs
+++ Program.cs
@@ -10,6 +10,7 @@ using DesignPatterns.Decorator;
 using DesignPatterns.FacetedB;
 using DesignPatterns.FactoryMethod;
 using DesignPatterns.Singleton;
+using DesignPatterns.Strategy;
 
 namespace DesignPatterns
 {
@@ -167,6 +168,28 @@ namespace DesignPatterns
             Console.WriteLine(product);
 
             // ********* Command Pattern Usage: END
+
+            // -------------------------------------------------------------------------
+            Console.WriteLine("\n-------------------------------\n");
+
+            // ********* Strategy Pattern Usage: START
+
+            var reports = new List<DeveloperReport>
+            {
+                new DeveloperReport {Id = 1, Name = "Dev1", Level = DeveloperLevel.Senior, HourlyRate = 30.5, WorkingHours = 160 },
+                new DeveloperReport { Id = 2, Name = "Dev2", Level = DeveloperLevel.Junior, HourlyRate = 20, WorkingHours = 120 },
+                new DeveloperReport { Id = 3, Name = "Dev3", Level = DeveloperLevel.Senior, HourlyRate = 32.5, WorkingHours = 130 },
+                new DeveloperReport { Id = 4, Name = "Dev4", Level = DeveloperLevel.Junior, HourlyRate = 24.5, WorkingHours = 140 }
+            };
+            var calculatorContext = new SalaryCalculator(new JuniorDeveloperSalaryCalculator());
+            var juniorTotal = calculatorContext.Calculate(reports);
+            Console.WriteLine($"Total amount for junior salaries is: {juniorTotal}");
+            calculatorContext.SetCalculator(new SeniorDeveloperSalaryCalculator());
+            var seniorTotal = calculatorContext.Calculate(reports);
+            Console.WriteLine($"Total amount for senior salaries is: {seniorTotal}");
+            Console.WriteLine($"Total cost for all the salaries is: {juniorTotal + seniorTotal}");
+
+            // ********* Strategy Pattern Usage: END
         }
     }
 }
