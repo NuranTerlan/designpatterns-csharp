b38bd24693c08e095a96616034247764827f32a9
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-03-16 23:02:26 +0400

    Create SalaryCalculator.cs

---
+++ Strategy/SalaryCalculator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace DesignPatterns.Strategy
+{
+    public class SalaryCalculator
+    {
+        private ISalaryCalculator _calculator;
+
+        public SalaryCalculator(ISalaryCalculator calculator)
+        {
+            _calculator = calculator;
+        }
+
+        public void SetCalculator(ISalaryCalculator calculator) => _calculator = calculator;
+        public double Calculate(IEnumerable<DeveloperReport> reports) => 
+            _calculator.CalculateTotalSalary(reports);
+    }
+}
