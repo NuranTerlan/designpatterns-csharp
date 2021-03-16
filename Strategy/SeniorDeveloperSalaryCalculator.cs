004962d2ca7ede3c4aa8b2f537b802447b1bd073
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-03-16 23:02:28 +0400

    Create SeniorDeveloperSalaryCalculator.cs

---
+++ Strategy/SeniorDeveloperSalaryCalculator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DesignPatterns.Strategy
+{
+    public class SeniorDeveloperSalaryCalculator : ISalaryCalculator
+    {
+        public double CalculateTotalSalary(IEnumerable<DeveloperReport> reports) =>
+            reports.Where(r => r.Level == DeveloperLevel.Senior)
+                .Select(r => r.CalculateSalary() * 1.2)
+                .Sum();
+    }
+}
