2285f6082acd9422dfe32848e90618d7db2e499f
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-03-16 23:02:24 +0400

    Create JuniorDeveloperSalaryCalculator.cs

---
+++ Strategy/JuniorDeveloperSalaryCalculator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DesignPatterns.Strategy
+{
+    public class JuniorDeveloperSalaryCalculator : ISalaryCalculator
+    {
+        public double CalculateTotalSalary(IEnumerable<DeveloperReport> reports) =>
+            reports.Where(r => r.Level == DeveloperLevel.Junior)
+                .Select(r => r.CalculateSalary())
+                .Sum();
+    }
+}
