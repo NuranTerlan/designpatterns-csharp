b4f86cfd07da5c69c7a40b8def947e07ee669db3
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-03-17 22:59:50 +0400

    Create ShippingService.cs

---
+++ Facade/ShippingService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DesignPatterns.Facade
+{
+    public class ShippingService
+    {
+        private Order _order;
+
+        public void AcceptOrder(Order order)
+        {
+            _order = order;
+        }
+
+        public void CalculateShippingExpenses()
+        {
+            _order.ShippingPrice = 15.5;
+        }
+
+        public void ShipOrder()
+        {
+            Console.WriteLine(_order);
+            Console.WriteLine($"Order is being shipped to {_order.ShippingAddress}");
+        }
+    }
+}
