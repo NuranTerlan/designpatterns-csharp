02579cbaa9b9a3d51a1e2dc1607185732067d580
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-03-17 23:12:18 +0400

    Create Facade.cs

---
+++ Facade/Facade.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DesignPatterns.Facade
+{
+    public class Facade
+    {
+        private readonly OnlineRestaurant _restaurant;
+        private readonly ShippingService _shippingService;
+
+        public Facade(OnlineRestaurant restaurant, ShippingService shippingService)
+        {
+            _restaurant = restaurant;
+            _shippingService = shippingService;
+        }
+
+        public void OrderFood(IList<Order> orders)
+        {
+            foreach (var order in orders)
+            {
+                _restaurant.AddOrderToCart(order);
+            }
+            _restaurant.CompleteOrders();
+            foreach (var order in orders)
+            {
+                _shippingService.AcceptOrder(order);
+                _shippingService.ShipOrder();
+            }
+
+            Console.WriteLine($"The total amount of orders: ${orders.Sum(o => o.FullPrice)}");
+        }
+    }
+}
