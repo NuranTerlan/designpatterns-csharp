44bbac7b318dc5b4dbc0d7d314c0ffec9624838b
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-03-17 22:59:46 +0400

    Create Order.cs

---
+++ Facade/Order.cs
@@ -0,0 +1,18 @@
+﻿namespace DesignPatterns.Facade
+{
+    public class Order
+    {
+        public string DishName { get; set; }
+        public double DishPrice { get; set; }
+        public string User { get; set; }
+        public string ShippingAddress { get; set; }
+        public double ShippingPrice { get; set; }
+
+        private double FullPrice => DishPrice + ShippingPrice;
+
+        public override string ToString()
+        {
+            return $"{DishName} has been ordered by {User}\nThe full price is ${FullPrice}";
+        }
+    }
+}
