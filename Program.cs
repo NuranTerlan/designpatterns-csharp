2e1b37916184c9e684b47fa53b6df255ca4b7f8d
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-03-17 23:12:09 +0400

    get rid of unnecessary business logic inside of the main

--- Program.cs
+++ Program.cs
@@ -199,17 +199,13 @@ namespace DesignPatterns
 
             var restaurant = new OnlineRestaurant();
             var shippingService = new ShippingService();
+
+            var facade = new Facade.Facade(restaurant, shippingService);
+
             var chickenOrder = new Order() { DishName = "Chicken with rice", DishPrice = 20.0, User = "User1", ShippingAddress = "Random street 123" };
             var sushiOrder = new Order() { DishName = "Sushi", DishPrice = 52.0, User = "User2", ShippingAddress = "More random street 321" };
-            restaurant.AddOrderToCart(chickenOrder);
-            restaurant.AddOrderToCart(sushiOrder);
-            restaurant.CompleteOrders();
-            shippingService.AcceptOrder(chickenOrder);
-            shippingService.CalculateShippingExpenses();
-            shippingService.ShipOrder();
-            shippingService.AcceptOrder(sushiOrder);
-            shippingService.CalculateShippingExpenses();
-            shippingService.ShipOrder();
+
+            facade.OrderFood(new List<Order> {chickenOrder, sushiOrder});
 
             // ********* Facade Pattern Usage: END
         }
