71266063d2ac4dad37d6b54aeeba034d39707f99
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-03-17 22:59:43 +0400

    Create OnlineRestaurant.cs

---
+++ Facade/OnlineRestaurant.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DesignPatterns.Facade
+{
+    public class OnlineRestaurant
+    {
+        private readonly IList<Order> _cart;
+
+        public OnlineRestaurant()
+        {
+            _cart = new List<Order>();
+        }
+
+        public OnlineRestaurant(IList<Order> cart)
+        {
+            _cart = cart;
+        }
+
+        public void AddOrderToCart(Order order)
+        {
+            _cart.Add(order);
+        }
+
+        public void RemoveOrderFromCart(Order order)
+        {
+            _cart.Remove(order);
+        }
+
+        public void CompleteOrders()
+        {
+            Console.WriteLine("Orders completed. Dispatch in progress...");
+        }
+    }
+}
