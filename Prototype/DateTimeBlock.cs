8373a30acebb770e113567fdf7538da92d85fbf3
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-03-23 23:41:31 +0400

    Create DateTimeBlock.cs

---
+++ Prototype/DateTimeBlock.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DesignPatterns.Prototype
+{
+    public class DateTimeBlock : IBlock
+    {
+        public DateTime DateTime { get; set; }
+        public string Format { get; set; }
+
+        public string Render => DateTime.ToString(Format);
+
+        public IBlock Clone() => new DateTimeBlock
+        {
+            DateTime = DateTime,
+            Format = Format
+        };
+
+        public override string ToString()
+        {
+            return $"Right-aligned datetime - {Render} ({Format})";
+        }
+    }
+}
