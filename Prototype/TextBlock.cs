7d0fccc0f6ab1b84f03ef4e6e7c13b6b141a538b
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-03-23 23:41:39 +0400

    Create TextBlock.cs

---
+++ Prototype/TextBlock.cs
@@ -0,0 +1,16 @@
+﻿namespace DesignPatterns.Prototype
+{
+    public class TextBlock : IBlock
+    {
+        public string Content { get; set; }
+
+        public string Render => Content;
+
+        public IBlock Clone() => new TextBlock {Content = Content};
+
+        public override string ToString()
+        {
+            return $"Left-aligned text - {Render}";
+        }
+    }
+}
