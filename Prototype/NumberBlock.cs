46e6bb290d22addd2bc1aaf3ca9c3e6e3fcfea3c
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-03-23 23:41:36 +0400

    Create NumberBlock.cs

---
+++ Prototype/NumberBlock.cs
@@ -0,0 +1,16 @@
+﻿namespace DesignPatterns.Prototype
+{
+    public class NumberBlock : IBlock
+    {
+        public int Number { get; set; }
+
+        public string Render => Number.ToString();
+
+        public IBlock Clone() => new NumberBlock {Number = Number};
+
+        public override string ToString()
+        {
+            return $"Right-aligned number - {Render}";
+        }
+    }
+}
