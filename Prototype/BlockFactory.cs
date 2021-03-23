c5dbcaae90c06865ec3ece62dff126314ccdc246
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-03-23 23:41:28 +0400

    Create BlockFactory.cs

---
+++ Prototype/BlockFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DesignPatterns.Prototype
+{
+    public class BlockFactory
+    {
+        public static IBlock Create(string content)
+        {
+            if (DateTime.TryParse(content, out var date))
+            {
+                return new DateTimeBlock
+                {
+                    Format = "dd/MM/yyyy",
+                    DateTime = date
+                };
+            }
+            else if (int.TryParse(content, out var number))
+            {
+                return new NumberBlock {Number = number};
+            }
+
+            return new TextBlock {Content = content};
+        }
+    }
+}
