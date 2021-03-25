02936af7e4b4e8ef31a44f9806c30039d18fd643
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-03-25 23:28:00 +0400

    bird's eye usage

---
+++ Proxy/ConcretionProxy.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DesignPatterns.Proxy
+{
+    public class ConcretionProxy : IAbstraction
+    {
+        // hide concretion, if we inject it we already exposed it
+        // compile time decision
+        Concretion concretion = new Concretion();
+
+        // simulate/hook/intercept/interupt communication with Concretion
+    }
+}
