8b1252e9b53dcdefbfb61aa576b67ca183b1eeac
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-03-25 23:28:20 +0400

    real usage for restriction

---
+++ Proxy/Protection-Proxy/ProtectedSettings.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DesignPatterns.Proxy
+{
+    public class ProtectedSettings : ISettings
+    {
+        private readonly AuthService _authService;
+        private readonly Settings _config;
+
+        public ProtectedSettings(AuthService authService)
+        {
+            _authService = authService;
+            _config = new Settings("config");
+        }
+
+        public string GetConfig()
+        {
+            if (!_authService.Allowed)
+            {
+                return "Not Allowed!";
+            }
+
+            return _config.GetConfig();
+        }
+    }
+}
