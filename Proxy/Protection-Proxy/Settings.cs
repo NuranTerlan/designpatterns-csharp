f6ee9d96e04844809e1e3e28752b92de00305e30
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-03-25 23:28:22 +0400

    Create Settings.cs

---
+++ Proxy/Protection-Proxy/Settings.cs
@@ -0,0 +1,11 @@
+﻿namespace DesignPatterns.Proxy
+{
+    public class Settings : ISettings
+    {
+        private readonly string _config;
+
+        public Settings(string config) => _config = config;
+
+        public string GetConfig() => _config;
+    }
+}
