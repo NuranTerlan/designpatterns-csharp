74aabdc89c0c834508576f0aa1225c5e6438d7e5
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-03-25 23:27:56 +0400

    bird's eye usage

---
+++ Proxy/ConcretionDecorator.cs
@@ -0,0 +1,16 @@
+﻿namespace DesignPatterns.Proxy
+{
+    public class ConcretionDecorator : IAbstraction
+    {
+        private readonly IAbstraction _abstraction;
+
+        // concretion is known
+        // runtime decision
+        public ConcretionDecorator(IAbstraction abstraction)
+        {
+            _abstraction = abstraction;
+        }
+
+        // override concretion functionality
+    }
+}
