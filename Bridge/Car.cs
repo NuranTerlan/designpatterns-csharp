7ba2160ec6305c6bd9e2a5131e81a1703c56ab1a
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-14 23:55:26 +0400

    Create Car.cs

---
+++ Bridge/Car.cs
@@ -0,0 +1,15 @@
+﻿namespace DesignPatterns.Bridge
+{
+    /// <summary>
+    /// Car class inherits from Vehicle class, and it's a lower level abstraction in
+    /// the bridge design pattern
+    /// </summary>
+    public class Car : Vehicle
+    {
+        public Car(Make make) : base(make)
+        {
+        }
+
+        public override bool DrivingLicenseApproved(string driver) => driver == "has license";
+    }
+}
