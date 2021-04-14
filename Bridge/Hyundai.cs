105aa82ab24bf878853a4285a83310743fbd30e0
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-14 23:55:28 +0400

    Create Hyundai.cs

---
+++ Bridge/Hyundai.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace DesignPatterns.Bridge
+{
+    public class Hyundai : Make
+    {
+        public override void PerformRitual() => Console.WriteLine("Great Santa Fe");
+
+        public override void StartEngine() => Console.WriteLine("Hold the start/stop button once");
+    }
+}
