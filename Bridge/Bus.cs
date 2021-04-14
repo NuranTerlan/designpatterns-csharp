4cf61f6e8d588c8b9cc69296816b320cdd2d024b
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-14 23:55:23 +0400

    Create Bus.cs

---
+++ Bridge/Bus.cs
@@ -0,0 +1,16 @@
+﻿namespace DesignPatterns.Bridge
+{
+    /// <summary>
+    /// Bus class inherits from Vehicle class, and it's a lower level abstraction in
+    /// the bridge design pattern
+    /// </summary>
+    public class Bus : Vehicle
+    {
+        public Bus(Make make) : base(make)
+        {
+        }
+
+        public override bool DrivingLicenseApproved(string driver) =>
+            driver == "has special bus license";
+    }
+}
