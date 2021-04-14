53544e1e6229504ea058c7656b041853dc416063
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-14 23:55:37 +0400

    Create Nissan.cs

---
+++ Bridge/Nissan.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DesignPatterns.Bridge
+{
+    public class Nissan : Make
+    {
+        public override void PerformRitual() => Console.WriteLine("Hit the car");
+
+        public override void StartEngine() => Console.WriteLine("Fix the wiring\n" +
+                                                                "Put the key" +
+                                                                "Turn the key");
+    }
+}
