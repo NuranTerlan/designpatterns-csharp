14e949a9b775d4f97fa0aab734d9043736d3ef25
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-14 23:55:40 +0400

    Create Vehicle.cs

---
+++ Bridge/Vehicle.cs
@@ -0,0 +1,23 @@
+﻿namespace DesignPatterns.Bridge
+{
+    /// <summary>
+    /// Vehicle class is a top level abstraction in the bridge design pattern
+    /// </summary>
+    public abstract class Vehicle
+    {
+        private Make _make;
+
+        protected Vehicle(Make make)
+        {
+            _make = make;
+        }
+
+        public abstract bool DrivingLicenseApproved(string driver);
+
+        public void Power()
+        {
+            _make.PerformRitual();
+            _make.StartEngine();
+        }
+    }
+}
