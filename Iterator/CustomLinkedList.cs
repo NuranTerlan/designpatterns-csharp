0eb65528ab8b568a88b39a144003d1abe1773f8d
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-25 23:47:16 +0400

    Create CustomLinkedList.cs

---
+++ Iterator/CustomLinkedList.cs
@@ -0,0 +1,29 @@
+﻿namespace DesignPatterns.Iterator
+{
+    public class CustomLinkedList<T>
+    {
+        private CustomNode<T> Head { get; set; }
+
+        public CustomLinkedList() {}
+
+        public CustomLinkedList(T head)
+        {
+            Head = new CustomNode<T>(head);
+        }
+
+        public void Add(T value)
+        {
+            if (Head == null)
+            {
+                Head = new CustomNode<T>(value);
+                return;
+            }
+
+            Head.Append(value);
+        }
+
+        public T Get(int i) => Head.Get(i);
+
+        public CustomLinkedIterator<T> Iterator => new CustomLinkedIterator<T>(Head);
+    }
+}
