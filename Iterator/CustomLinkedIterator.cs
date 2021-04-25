827949bf20d607daa8170914ce39ed671f0ccade
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-25 23:47:12 +0400

    Create CustomLinkedIterator.cs

---
+++ Iterator/CustomLinkedIterator.cs
@@ -0,0 +1,27 @@
+﻿namespace DesignPatterns.Iterator
+{
+    public class CustomLinkedIterator<T>
+    {
+        private readonly CustomNode<T> _root;
+        private CustomNode<T> _current;
+
+        public CustomLinkedIterator(CustomNode<T> root)
+        {
+            _root = _current = root;
+        }
+
+        public T Next()
+        {
+            var value = _current.Value;
+            _current = _current.Next;
+            return value;
+        }
+
+        public bool Complete => _current == null;
+
+        public void Reset()
+        {
+            _current = _root;
+        }
+    }
+}
