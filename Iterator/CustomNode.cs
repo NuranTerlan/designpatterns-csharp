e50d0739cfcffe410d212289dc53d9e1e8480544
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-04-25 23:47:19 +0400

    Create CustomNode.cs

---
+++ Iterator/CustomNode.cs
@@ -0,0 +1,27 @@
+﻿namespace DesignPatterns.Iterator
+{
+    public class CustomNode<T>
+    {
+        public T Value { get; set; }
+        public CustomNode<T> Next { get; set; }
+
+        public CustomNode(T value)
+        {
+            Value = value;
+            Next = null;
+        }
+
+        public void Append(T value)
+        {
+            if (Next == null)
+            {
+                Next = new CustomNode<T>(value);
+                return;
+            }
+
+            Next.Append(value);
+        }
+
+        public T Get(int i) => i == 0 ? Value : Next.Get(--i);
+    }
+}
